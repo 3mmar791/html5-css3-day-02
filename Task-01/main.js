115f49c352518b66bce00cc670d412045ae4da66
Ammar Araby <ammararbie424@gmail.com>
2024-09-09 12:20:40 +0300

    edit canvas

--- Task-01/main.js
+++ Task-01/main.js
@@ -1,22 +1,32 @@
-const canvas = document.getElementById("myCanvas");
-const context = canvas.getContext("2d");
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 function drawRandomCircles() {
-  const color = document.getElementById("circleColor").value;
+  var colorVlue = document.getElementById("circleColor").value;
+  var color = document.getElementById("circleColor");
 
-  context.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
-  this.style.backgroundColor = color;
+  color.onchange = function (e) {
+    if (e.target.value === colorVlue) {
+      return;
+    } else {
+      context.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+      colorVlue = e.target.value;
+    }
+  };
+
+  this.style.backgroundColor = colorVlue; // styling the button with the same color
+
   for (let i = 0; i < 100; i++) {
-    const x = getRandomInt(0, canvas.width);
-    const y = getRandomInt(0, canvas.height);
-    const radius = getRandomInt(10, 40);
+    var x = getRandomInt(0, canvas.width);
+    var y = getRandomInt(0, canvas.height);
+    var radius = getRandomInt(10, 40);
 
     context.beginPath();
     context.arc(x, y, radius, 0, Math.PI * 2);
-    context.strokeStyle = color;
+    context.strokeStyle = colorVlue;
     context.stroke();
   }
 }
